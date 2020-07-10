d64143a8145c44276d04f535798ecdb7e32c9976
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2020-07-09 22:54:47 -0300

    Delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao 
@@ -13,6 +14,7 @@ public interface SellerDao
 	
 	List<Seller> findAll();
 	
+	List<Seller> findByDepartment(Department department);
 	
 
 }
